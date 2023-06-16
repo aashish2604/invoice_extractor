121f2ad2e3fefb45a080f37dabd36c0c4eefcdc8
aashish2604 <aashish.260401@gmail.com>
2023-06-17 04:27:48 +0530

    finalised extract.js

--- extract.js
+++ extract.js
@@ -1,21 +1,11 @@
 const PDFServicesSdk = require("@adobe/pdfservices-node-sdk");
 const fs = require("fs");
 const AdmZip = require("adm-zip");
-const Parser = require("./parser");
+const Parser = require("./utilities/parser");
 const converter = require("json-2-csv");
+const sorter = require("./utilities/helper_function");
 
-
-function sortStringArrayByNumericValue(strings) {
-  const numericSort = (a, b) => {
-    const numericA = parseInt(a.match(/\d+/)[0]);
-    const numericB = parseInt(b.match(/\d+/)[0]);
-    return numericA - numericB;
-  };
-
-  return strings.sort(numericSort);
-}
-
-const API_OUTPUT_DIRECTORY="./outputs/api_response";
+const API_OUTPUT_DIRECTORY = "./outputs/api_response";
 
 const credentials =
   PDFServicesSdk.Credentials.serviceAccountCredentialsBuilder()
@@ -25,26 +15,23 @@ const credentials =
 // Create an ExecutionContext using credentials
 const executionContext = PDFServicesSdk.ExecutionContext.create(credentials);
 
-///////
-
 const INPUT_DIRECTORY = "./inputs";
 
 let fileNames = fs.readdirSync(INPUT_DIRECTORY);
-sortStringArrayByNumericValue(fileNames);
+sorter.sortFileNames(fileNames);
 
 const parser = new Parser();
 
 async function parseDocuments() {
   for (const itr in fileNames) {
-
     const fileName = fileNames[itr];
-    const fileNameWithoutExtension=fileName.split(".")[0];
+    const fileNameWithoutExtension = fileName.split(".")[0];
     const FILEPATH = INPUT_DIRECTORY + "/" + fileName;
 
-    const API_OUTPUT_PATH = API_OUTPUT_DIRECTORY+"/ExtractedTextFrom";
+    const API_OUTPUT_PATH = API_OUTPUT_DIRECTORY + "/ExtractedTextFrom";
     const OUTPUT_ZIP = API_OUTPUT_PATH + fileNameWithoutExtension + ".zip";
 
-    if(fs.existsSync(OUTPUT_ZIP)) fs.unlinkSync(OUTPUT_ZIP);
+    if (fs.existsSync(OUTPUT_ZIP)) fs.unlinkSync(OUTPUT_ZIP);
 
     // Create a new operation instance.
     const extractPDFOperation = PDFServicesSdk.ExtractPDF.Operation.createNew(),
@@ -68,30 +55,26 @@ async function parseDocuments() {
       .execute(executionContext)
       .then((result) => result.saveAsFile(OUTPUT_ZIP))
       .then(() => {
-        console.log(`\nSuccessfully extracted data from ${fileName} ✔\n`);
         let zip = new AdmZip(OUTPUT_ZIP);
         let jsondata = zip.readAsText("structuredData.json");
 
         let data = JSON.parse(jsondata);
         parser.parseApiResponse(data);
+        console.log(`\nSuccessfully extracted data from ${fileName} ✔\n`);
       })
       .catch((err) => console.log(err));
   }
 }
 
-parseDocuments().then(()=>{
-    const csvFilePath="outputs/ExtractedData.csv";
+parseDocuments().then(() => {
+  const csvFilePath = "outputs/ExtractedData.csv";
 
-    if(fs.existsSync(csvFilePath)) fs.unlinkSync(csvFilePath);
+  if (fs.existsSync(csvFilePath)) fs.unlinkSync(csvFilePath);
 
-    converter.json2csv(parser.collectiveParsedReponse).then((response)=>{
-        fs.writeFile(csvFilePath, response, "utf-8", (err) => {
-            if (err) console.log(err);
-            else console.log(`\nHurray....Data is saved in ${csvFilePath} ✔✔\n`);
-          });
-    })
-
-    
+  converter.json2csv(parser.collectiveParsedReponse).then((response) => {
+    fs.writeFile(csvFilePath, response, "utf-8", (err) => {
+      if (err) console.log(err);
+      else console.log(`\nAll Done 👍....Data is saved in ${csvFilePath} ✔✔\n`);
+    });
+  });
 });
-
-// Execute the operation
